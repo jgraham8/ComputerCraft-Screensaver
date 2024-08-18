ba382eb484b677d1e98cd1a5b63c1c6f133053c9
Johnathan Graham <johnathanjbgraham@gmail.com>
2024-08-18 17:59:33 +0100

    first test

--- Screensaver.lua
+++ Screensaver.lua
@@ -1,1 +1,40 @@
+local mon = peripheral.find("monitor")
 
+local timeWindow = window.create(term.current(), 1,1,65,30)
+timeWindow.setBackgroundColour(colours.white)
+timeWindow.setTextColour(colours.black)
+term.redirect(timeWindow)
+
+function drawWindow()
+    timeWindow.setCursorPos(1,1)
+    timeWindow.clear()
+    print(
+        textutils.tabulate(
+            colours.black,
+            {
+                "Ingame:",
+                textutils.formatTIme(os.time(), true),
+                checkTime( os.time() )
+            },
+            colours.black,
+            {
+                "real:",
+                textutils.formatTIme(os.time("local"), true),
+                checkTime( os.time("local") )
+            }
+        )
+    )
+end
+
+function checkTime( t )
+    if t > 6 and t < 18 then
+        return "day"
+    end
+    return "night"
+end
+
+while true do
+    drawWindow()
+    sleep(0.75)
+    timeWindow.redraw()
+end
